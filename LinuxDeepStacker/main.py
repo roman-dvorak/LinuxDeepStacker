532ed9c0464e04b5d481b66856a75bfd57da7c66
Roman Dvořák <roman-dvorak@email.cz>
2016-01-03 13:56:29 +0100

    separe 'loadproject' class to own file, close file procedure

--- LinuxDeepStacker/main.py
+++ LinuxDeepStacker/main.py
@@ -11,16 +11,19 @@ import time
 import datetime
 import h5py
 import configparser
+import numpy as np
+from ui import LoadProject
+#from work import *
 
-print "hallo"
-gettext.bindtextdomain('cs_CZ', '')
-#gettext.find('cs_CZ')
+gettext.bindtextdomain('cs_CZ')
 gettext.textdomain('cs_CZ')
 _ = gettext.gettext
 
 
 ##########################################################################################################
 ##########################################################################################################    
+##### Třída starající se o práci s projektem .. ukladaní, toření, otevírání načítání dat ....
+##############
 
 class ProjectClass():
     def __init__(self, arg):
@@ -49,24 +52,24 @@ class ProjectClass():
                 self.load(x)
 
     def new(self):
-        self.ProjectCreationDate = str(datetime.datetime.utcnow())
-        self.File = h5py.File(self.ProjectFile)
+        self.ProjectCreationDate = datetime.datetime.utcnow()
+        self.File = h5py.File(self.ProjectFile, 'w')
         self.FileConfig = self.File.require_dataset("config", (10,2), maxshape=(None, 2), dtype = h5py.special_dtype(vlen=unicode))
         self.FileConfig.attrs.modify("ProjectName", self.ProjectName)
-        self.FileConfig.attrs.modify("ProjectCreationDate", self.ProjectCreationDate)
-        self.FileConfig.attrs.modify("ProjectLastOpen", self.ProjectCreationDate)
+        self.FileConfig.attrs.modify("ProjectCreationDate", str(self.ProjectCreationDate))
+        self.FileConfig.attrs.modify("ProjectLastOpen", [str(self.ProjectCreationDate)])
         self.FileConfig.attrs.modify("test", [10,23,023,034])
         self.File.flush()
-        
 
     def load(self, path):
         print _("Loading varibales from %s")%(path)
-        self.File = h5py.File(path)
+        self.File = h5py.File(path, 'r+')
         self.FileConfig = self.File['config']
         self.ProjectFile = path
         self.ProjectName = self.FileConfig.attrs['ProjectName']
         self.ProjectCreationDate = self.FileConfig.attrs['ProjectCreationDate']
         self.ProjectLastOpen = self.FileConfig.attrs['ProjectLastOpen']
+        self.FileConfig.attrs['ProjectLastOpen'] = np.append(self.ProjectLastOpen, str(datetime.datetime.utcnow()))
 
     def save(self):
         print _("Saving varibales")
@@ -74,123 +77,14 @@ class ProjectClass():
     def save_as(self):
         print _("Saving as varibales")
 
+    def OnClose(self):
+        self.File.flush()
+        self.File.close()
 
 ##########################################################################################################
 ##########################################################################################################
-
-class LoadProject(wx.Frame):
-    def __init__(self, parent=None, id=-1, title=_("Linux deep stacker | ") + _("Výběr projektu"), prj = None):
-        super(LoadProject, self).__init__(parent, title=title, size=(557, 410), style= wx.CLOSE_BOX | wx.FRAME_FLOAT_ON_PARENT)
-        self.parent = parent
-        self.prj = prj
-        self.status = False
-        panel = wx.Panel(self)
-        sizer = wx.GridBagSizer(1, 1)
-
-        box = wx.BoxSizer(wx.VERTICAL)
-        HeaderImage = wx.StaticBitmap(panel, bitmap=wx.Bitmap("LinuxDeepStacker/media/headerB.png"))
-
-        BtnOpen = wx.Button(panel, label=_("Open"))
-        BtnNew = wx.Button(panel, label=_("New"))
-        BtnDone = wx.Button(panel, label=_("OK"))
-        BtnCancel = wx.Button(panel, label=_("Cancel"))
-        self.TcLocation = wx.TextCtrl(panel, -1)
-        self.TcLocation.SetEditable(False)
-        self.TcProjectName = wx.TextCtrl(panel, -1)
-        self.TcProjectName.SetEditable(False)
-        BtnRecently = wx.Choice(panel)
-        LabLocation = wx.StaticText(panel, -1, _('  ProjectFile:  '))
-        LabPrjName = wx.StaticText(panel, -1, _('  ProjectName:  '))
-        self.LabInfo = wx.StaticText(panel, -1, _('  Info:  ') + " " + _('from %s to %s, %d Lights, %d Flats and %d Darks shots.')%('2015-10-10t23:34:45UT', '2015-10-10t23:50:45UT', 530, 10, 10))
-        self.LabInfoR2 = wx.StaticText(panel, -1, _('from %s to %s, %d Lights, %d Flats and %d Darks shots.')%('2015-10-10t23:34:45UT', '2015-10-10t23:50:45UT', 530, 10, 10))
-
-        sizer.Add(HeaderImage, pos=(0, 0), span=(1, 9), flag = wx.RIGHT|wx.TOP)
-        sizer.Add(BtnOpen, pos=(1, 1), span=(1, 1), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
-        sizer.Add(BtnNew, pos=(1, 2), span=(1, 1), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
-        sizer.Add(BtnRecently, pos=(1, 3), span=(1, 4), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
-        sizer.Add(LabLocation, pos=(3, 0), flag= wx.LEFT)
-        sizer.Add(self.TcLocation, pos=(3, 1), span=(1, 7), flag = wx.EXPAND |wx.TOP | wx.LEFT | wx.BOTTOM)
-        sizer.Add(LabPrjName, pos=(4, 0), flag=wx.LEFT)
-        sizer.Add(self.TcProjectName, pos=(4, 1), span=(1, 7), flag = wx.EXPAND |wx.TOP | wx.LEFT | wx.BOTTOM)
-        sizer.Add(self.LabInfo, pos=(5, 0), span=(1, 9), flag = wx.TOP | wx.LEFT)
-        sizer.Add(self.LabInfoR2, pos=(6, 1), span=(1, 8), flag = wx.TOP | wx.LEFT)
-        sizer.Add(BtnDone, pos=(8, 5), span=(1, 1), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
-        sizer.Add(BtnCancel, pos=(8, 6), span=(1, 1), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
-
-        self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
-        self.Bind(wx.EVT_BUTTON, self.OnCloseWindow, BtnCancel)
-        self.Bind(wx.EVT_BUTTON, self.OnCreateProject, BtnNew)
-        self.Bind(wx.EVT_BUTTON, self.OnOpenProject, BtnOpen)
-        self.Bind(wx.EVT_BUTTON, self.OnLoadProject, BtnDone)
-
-        #sizer.Add(HeaderImage, pos=(3, 4), flag=wx.RIGHT|wx.BOTTOM, border=5)
-        panel.SetSizer(sizer)
-
-        self.MakeModal(True)
-        self.Show(True)
-        self.Centre()
-
-    def OnCloseWindow(self, widget=None):
-        self.Close()
-        self.parent.Close()
-        self.status = False
-        self.MakeModal(False)
-
-    def OnCreateProject(self, widget=None):
-        dlg = wx.FileDialog(
-            self, message="Vyber projektovy soubor", wildcard = "ProjectFile (*.lds,*.ldsa)|*.lds;*.ldsa",
-            style=wx.SAVE | wx.OVERWRITE_PROMPT | wx.CHANGE_DIR)
-        if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPath()
-            if ".lds" not in path.lower():
-                path = path+".lds"
-            print "You chose the following file(s):",
-            print path
-            self.TcLocation.SetEditable(False)
-            self.TcLocation.SetValue(path)
-            self.TcProjectName.SetEditable(True)
-            self.TcProjectName.SetValue(_("Projekt bez názvu z %s.") %str(datetime.datetime.utcnow()))
-            self.prj.ProjectFile = self.TcLocation.GetValue()
-            self.prj.ProjectName = self.TcProjectName.GetValue()
-            self.prj.ProjectLoadType = 4
-        dlg.Destroy()
-        
-
-    def OnOpenProject(self, widget=None):
-        dlg = wx.FileDialog(
-            self, message="Vyber projektovy soubor", wildcard = "ProjectFile (*.lds,*.ldsa)|*.lds;*.ldsa",
-            style=wx.OPEN | wx.CHANGE_DIR)
-        if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPath()
-            print "You chose the following file(s):",
-            print path
-            self.TcLocation.SetEditable(False)
-            self.TcLocation.SetValue(path)
-            self.TcProjectName.SetEditable(False)
-            self.TcProjectName.SetValue(path)
-            self.prj.load(path = path)
-        dlg.Destroy()
-
-    def OnLoadProject(self, widget=None):
-        self.prj.ProjectFile = self.TcLocation.GetValue()
-        self.prj.ProjectName = self.TcProjectName.GetValue()
-        self.MakeModal(False)
-        self.status = True
-        self.Destroy()
-        if self.prj.ProjectLoadType == 4:
-            self.prj.new()
-        return self.status
-        
-    def UpdateUI(self, widget=None):
-        print "Update load screen"
-
-    def status(self):
-        return self.status
-
-
-
-##########################################################################################################
-##########################################################################################################
+###### První panel hl. okna
+#############
 
 class Page_project():
     def __init__(self, parrent, notebook):
@@ -221,7 +115,6 @@ class Page_project():
         LeftPanel_vbox.Add(self.HeaderImage)
         LeftPanel_vbox.Add(wx.StaticText(self.leftPanel, -1, _("Project name: %s") % str(self.parent.prj.ProjectName), (10,193)))
 
-
         self.splitter.SplitVertically (self.leftPanel, self.rightPanel, 315)
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.splitter, 1, wx.EXPAND)
@@ -232,6 +125,8 @@ class Page_project():
 
 #########################################################################################################
 #########################################################################################################
+###### Tvoří základní okno
+#############
 
 class LinuxDeepStacker(wx.Frame):
     def __init__(self, parent=None, id=-1, title=_("Linux deep stacker | ") + "Err.: " + _("Není otevřen žádný projekt"), prj = None, app = None):
@@ -239,6 +134,7 @@ class LinuxDeepStacker(wx.Frame):
         self.prj = prj
         self.parent = parent
         wx.Frame.__init__(self, self.parent, id, title, size=(800, 500))
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
         panel = wx.Panel(self)
         notebook = wxfnb.FlatNotebook(panel, agwStyle= wxfnb.FNB_NO_X_BUTTON | wxfnb.FNB_NAV_BUTTONS_WHEN_NEEDED | wxfnb.FNB_SMART_TABS  )
         sizer = wx.BoxSizer(wx.VERTICAL)
@@ -253,20 +149,18 @@ class LinuxDeepStacker(wx.Frame):
         self.Centre()
 
         if self.prj.ProjectName == None:
-            LoadProject(self, prj = prj)
+            LoadProject.LoadProject(self, prj = prj)
 
     def CreatePage(self, notebook, caption):
-        '''
-        Creates a simple :class:`Panel` containing a :class:`TextCtrl`.
-
-        :param `notebook`: an instance of `FlatNotebook`;
-        :param `caption`: a simple label.
-        '''
-
         p = wx.Panel(notebook)
         wx.StaticText(p, -1, caption, (20,20))
         wx.TextCtrl(p, -1, "", (20,40), (150,-1))
         return p
+
+    def OnClose(self, widget=None):
+        print ('Ukončuji program z události tlačítka')
+        self.prj.OnClose()
+        self.Destroy()
 
 ##########################################################################################################
 ##########################################################################################################
---
+++ LinuxDeepStacker/ui/LoadProject.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+import sys
+import wx
+import wx.lib.agw.flatnotebook as wxfnb
+import gettext
+import time
+import datetime
+import h5py
+import configparser
+
+
+gettext.bindtextdomain('cs_CZ')
+gettext.textdomain('cs_CZ')
+_ = gettext.gettext
+
+class LoadProject(wx.Frame):
+    def __init__(self, parent=None, id=-1, title=_("Linux deep stacker | ") + _("Výběr projektu"), prj = None):
+        super(LoadProject, self).__init__(parent, title=title, size=(557, 410), style= wx.CLOSE_BOX | wx.FRAME_FLOAT_ON_PARENT)
+        self.parent = parent
+        self.prj = prj
+        self.status = False
+        panel = wx.Panel(self)
+        sizer = wx.GridBagSizer(1, 1)
+
+        box = wx.BoxSizer(wx.VERTICAL)
+        HeaderImage = wx.StaticBitmap(panel, bitmap=wx.Bitmap("LinuxDeepStacker/media/headerB.png"))
+
+        BtnOpen = wx.Button(panel, wx.ID_OPEN)
+        BtnNew = wx.Button(panel, wx.ID_NEW)
+        BtnDone = wx.Button(panel, wx.ID_OK)
+        BtnCancel = wx.Button(panel, wx.ID_EXIT)
+        self.TcLocation = wx.TextCtrl(panel, -1)
+        self.TcLocation.SetValue(os.path.expanduser("~"))
+        self.TcLocation.SetEditable(False)
+        self.TcProjectName = wx.TextCtrl(panel, -1)
+        self.TcProjectName.SetEditable(False)
+        BtnRecently = wx.Choice(panel)
+        LabLocation = wx.StaticText(panel, -1, _('  ProjectFile:  '))
+        LabPrjName = wx.StaticText(panel, -1, _('  ProjectName:  '))
+        self.LabInfo = wx.StaticText(panel, -1, _('  Info:  ') + " " + _('from %s to %s, %d Lights, %d Flats and %d Darks shots.')%('2015-10-10t23:34:45UT', '2015-10-10t23:50:45UT', 530, 10, 10))
+        self.LabInfoR2 = wx.StaticText(panel, -1, _('from %s to %s, %d Lights, %d Flats and %d Darks shots.')%('2015-10-10t23:34:45UT', '2015-10-10t23:50:45UT', 530, 10, 10))
+
+        sizer.Add(HeaderImage, pos=(0, 0), span=(1, 9), flag = wx.RIGHT|wx.TOP)
+        sizer.Add(BtnOpen, pos=(1, 1), span=(1, 1), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
+        sizer.Add(BtnNew, pos=(1, 2), span=(1, 1), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
+        sizer.Add(BtnRecently, pos=(1, 3), span=(1, 4), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
+        sizer.Add(LabLocation, pos=(3, 0), flag= wx.LEFT)
+        sizer.Add(self.TcLocation, pos=(3, 1), span=(1, 7), flag = wx.EXPAND |wx.TOP | wx.LEFT | wx.BOTTOM)
+        sizer.Add(LabPrjName, pos=(4, 0), flag=wx.LEFT)
+        sizer.Add(self.TcProjectName, pos=(4, 1), span=(1, 7), flag = wx.EXPAND |wx.TOP | wx.LEFT | wx.BOTTOM)
+        sizer.Add(self.LabInfo, pos=(5, 0), span=(1, 9), flag = wx.TOP | wx.LEFT)
+        sizer.Add(self.LabInfoR2, pos=(6, 1), span=(1, 8), flag = wx.TOP | wx.LEFT)
+        sizer.Add(BtnCancel, pos=(8, 5), span=(1, 1), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
+        sizer.Add(BtnDone, pos=(8, 6), span=(1, 1), flag = wx.EXPAND | wx.LEFT | wx.RIGHT)
+
+        self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
+        self.Bind(wx.EVT_BUTTON, self.OnCloseWindow, BtnCancel)
+        self.Bind(wx.EVT_BUTTON, self.OnCreateProject, BtnNew)
+        self.Bind(wx.EVT_BUTTON, self.OnOpenProject, BtnOpen)
+        self.Bind(wx.EVT_BUTTON, self.OnLoadProject, BtnDone)
+
+        #sizer.Add(HeaderImage, pos=(3, 4), flag=wx.RIGHT|wx.BOTTOM, border=5)
+        panel.SetSizer(sizer)
+
+        self.MakeModal(True)
+        self.Show(True)
+        self.Centre()
+
+    def OnCloseWindow(self, widget=None):
+        self.Close()
+        self.parent.Close()
+        self.status = False
+        self.MakeModal(False)
+
+    def OnCreateProject(self, widget=None):
+        dlg = wx.FileDialog(
+            self, message="Vyber projektovy soubor", wildcard = "ProjectFile (*.lds,*.ldsa)|*.lds;*.ldsa",
+            style=wx.SAVE | wx.OVERWRITE_PROMPT | wx.CHANGE_DIR)
+        if dlg.ShowModal() == wx.ID_OK:
+            path = dlg.GetPath()
+            if ".lds" not in path.lower():
+                path = path+".lds"
+            print "You chose the following file(s):",
+            print path
+            self.TcLocation.SetEditable(False)
+            self.TcLocation.SetValue(path)
+            self.TcProjectName.SetEditable(True)
+            self.TcProjectName.SetValue(_("Projekt bez názvu z %s.") %str(datetime.datetime.utcnow()))
+            self.prj.ProjectFile = self.TcLocation.GetValue()
+            self.prj.ProjectName = self.TcProjectName.GetValue()
+            self.prj.ProjectLoadType = 4
+        dlg.Destroy()
+        
+    def OnOpenProject(self, widget=None):
+        dlg = wx.FileDialog(
+            self, message="Vyber projektovy soubor", wildcard = "ProjectFile (*.lds,*.ldsa)|*.lds;*.ldsa",
+            style=wx.OPEN | wx.CHANGE_DIR)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.prj.ProjectLoadType = 2
+            path = dlg.GetPath()
+            print "You chose the following file(s):",
+            print path
+            self.TcLocation.SetEditable(False)
+            self.TcLocation.SetValue(path)
+            self.TcProjectName.SetEditable(False)
+            self.TcProjectName.SetValue("Uneable to read project name, try continue")
+            self.prj.load(path = path)
+        dlg.Destroy()
+        self.MakeModal(False)
+        self.status = True
+        self.Destroy()
+
+    def OnLoadProject(self, widget=None):
+        if self.prj.ProjectLoadType != -1:
+            self.prj.ProjectFile = self.TcLocation.GetValue()
+            self.prj.ProjectName = self.TcProjectName.GetValue()
+            self.MakeModal(False)
+            self.status = True
+            self.Destroy()
+            if self.prj.ProjectLoadType == 4:
+                self.prj.new()
+            return self.status
+        
+    def UpdateUI(self, widget=None):
+        print "Update load screen"
+
+    def status(self):
+        return self.status
